7ad66d1b9429a26e5beba6f44372caac09f47372
Maksim Dahovnik <dahovnik@gmail.com>
2024-11-19 00:04:21 +0200

    feat: Open the editing form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,24 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({ isOpen, rootRef: containerRef, onChange: setIsOpen });
+
+	const handleArrowBtnClick = () => {
+		setIsOpen(!isOpen);
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleArrowBtnClick} />
+			<aside className={clsx(styles.container, { [styles.containerOpen]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
